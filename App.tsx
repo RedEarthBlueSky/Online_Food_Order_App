d31241b4e3417295bd955f77236b0ed1d257691b
Your <ian@isalt.digital>
2021-11-15 16:25:29 +0000

    linter and prettier setup

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World, this is App.tsx yeah!</Text>
       <StatusBar style="auto" />
     </View>
   );
